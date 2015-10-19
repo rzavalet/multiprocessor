5b324ce72e4f107fb6c3b82fb00c0661d50b061b
dario <dario.mx@gmail.com>
2015-10-19 10:25:30 -0500

    dario1

---
+++ dario/Multiprocessor/QueueTest.java
@@ -0,0 +1,25 @@
+\section{\textbf{LockFreeQueueTest}}
+
+\subsection{Particular Case (problem)}
+
+\subsection{Solution}
+
+\begin{lstlisting}[style=numbers]
+
+\end{lstlisting}
+\hfill
+
+\subsection{Experiment Description}
+
+\begin{itemize}
+  \item \C{testSequential}: 
+\end{itemize}
+
+\subsection{Observations and Interpretations}
+\subsubsection{Non atomic dequeue: referencing invalid registers}
+
+
+\begin{verbatim}
+\end{verbatim}
+\hfill
+
